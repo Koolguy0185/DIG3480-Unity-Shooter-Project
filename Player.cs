f348810cc78fc76918c0a12d6fa2e8b5929dc8fd
Antoine Holmes Jr. <130575550+Koolguy0185@users.noreply.github.com>
2025-03-29 12:45:40 -0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float playerSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float horizontalScreenLimit = 9.37f;
+    private float verticalScreenLimit = -3.6f;
+
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+    }
+
+    void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0,1,0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, verticalScreenLimit, 0);
+        }
+    }
+}
